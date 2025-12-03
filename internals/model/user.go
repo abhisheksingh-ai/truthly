c2dcf61ffc2f8965d3d784fc556718703aad21ac
Abhishek Singh <abhishek.singh@eroute.in>
2025-12-03 16:40:34 +0530

    fixed typo

--- internals/model/user.go
+++ internals/model/user.go
@@ -8,19 +8,20 @@ import (
 )
 
 type User struct {
-	UserId    string    `gorm:"column:UserId;primaryKey"`
-	FirstName string    `gorm:"column:FirstName"`
-	LastName  string    `gorm:"column:LastName"`
-	Age       string    `gorm:"column:Age"`
-	Gender    string    `gorm:"column:Gender"`
-	Country   string    `gorm:"column:Country"`
-	State     string    `gorm:"column:State"`
-	City      string    `gorm:"column:City"`
-	Address   string    `gorm:"column:Address"`
-	Email     string    `gorm:"column:Email"`
-	Password  string    `gorm:"column:Password"`
-	CreatedAt time.Time `gorm:"column:CreatedAt"`
-	UpdatedAt time.Time `gorm:"column:UpdatedAt"`
+	UserId       string    `gorm:"column:UserId;primaryKey"`
+	FirstName    string    `gorm:"column:FirstName"`
+	LastName     string    `gorm:"column:LastName"`
+	Age          string    `gorm:"column:Age"`
+	Gender       string    `gorm:"column:Gender"`
+	Country      string    `gorm:"column:Country"`
+	State        string    `gorm:"column:State"`
+	City         string    `gorm:"column:City"`
+	Address      string    `gorm:"column:Address"`
+	Email        string    `gorm:"column:Email"`
+	Password     string    `gorm:"column:Password"`
+	MobileNumber string    `gorm:"column:MobileNumber"`
+	CreatedAt    time.Time `gorm:"column:CreatedAt"`
+	UpdatedAt    time.Time `gorm:"column:UpdatedAt"`
 }
 
 func (User) TableName() string {
