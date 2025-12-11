13c34edbed7d7859ee7e9653f72a072f25a5147a
Abhishek Singh <asingh19082000@gmail.com>
2025-12-11 22:54:06 +0530

    S3Uploader added

--- internals/service/PostService.go
+++ internals/service/PostService.go
@@ -3,12 +3,13 @@ package service
 import (
 	"context"
 	"log/slog"
+	"mime/multipart"
 	"truthly/internals/dto"
 	"truthly/internals/repository"
 )
 
 type PostService interface {
-	UploadPost(ctx context.Context, imageUrl string) (*dto.ResponseDto, error)
+	UploadPost(ctx context.Context, file multipart.File) (*dto.ResponseDto, error)
 }
 
 type postService struct {
@@ -17,6 +18,7 @@ type postService struct {
 	commentRepo     *repository.CommentRepository
 	descriptionRepo *repository.DescriptionRepository
 	imageRepo       *repository.ImageRepository
+	s3Uploader      *S3Uploader
 }
 
 func GetPostService(
@@ -25,6 +27,7 @@ func GetPostService(
 	commentRepo *repository.CommentRepository,
 	descriptionRepo *repository.DescriptionRepository,
 	imageRepo *repository.ImageRepository,
+	s3Uploader *S3Uploader,
 ) PostService {
 	return &postService{
 		logger:          logger,
@@ -32,10 +35,22 @@ func GetPostService(
 		commentRepo:     commentRepo,
 		descriptionRepo: descriptionRepo,
 		imageRepo:       imageRepo,
+		s3Uploader:      s3Uploader,
 	}
 }
 
-func (s *postService) UploadPost(ctx context.Context, imageUrl string) (*dto.ResponseDto, error) {
+func (s *postService) UploadPost(ctx context.Context, file multipart.File) (*dto.ResponseDto, error) {
 	var result dto.ResponseDto
+
+	//1. Upload the image on s3 bucket and get url
+
+	//2. Insert row in Image table
+
+	// 3. Insert the description row in table like city, state etc
+
+	// 4. Analytic like, share, comment initially set 0
+
+	// 5. Comment initally nothing
+
 	return &result, nil
 }
--- internals/service/S3Uploader.go
+++ internals/service/S3Uploader.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"mime/multipart"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -23,4 +25,21 @@ func NewS3Uploader(bucket string) (*S3Uploader, error) {
 		Client:     s3.NewFromConfig(cfg),
 		BucketName: bucket,
 	}, nil
+}
+
+func (s *S3Uploader) UploadImage(file multipart.File, fileName string) (string, error) {
+	_, err := s.Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: &s.BucketName,
+		Key:    &fileName,
+		Body:   file,
+		ACL:    "public-read",
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	// generate the image url
+	url := fmt.Sprintf("https://%s.s3.amazonaws.com/%s", s.BucketName, fileName)
+	return url, nil
 }
