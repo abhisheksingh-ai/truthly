89e477efb5f0845ca72a5a421c099836e2cb16dc
Abhishek Singh <asingh19082000@gmail.com>
2025-12-14 08:41:07 +0530

    created FeedResponseDto, modefied the name of some dtos

---
+++ internals/dto/FeedResponseDto.go
@@ -0,0 +1,43 @@
+package dto
+
+import "time"
+
+type FeedResponseDto struct {
+	Items      []FeedItemDto `json:"items"`
+	Pagination PaginationDto `json:"pagination"`
+}
+
+type FeedItemDto struct {
+	ImageId  string `json:"imageId"`
+	ImageUrl string `json:"imageUrl"`
+	Caption  string `json:"caption"`
+
+	Location  LocationDto  `json:"location"`
+	Analytics AnalyticsDto `json:"analytics"`
+
+	RecentComments []CommentDto `json:"recentComments,omitempty"`
+
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type LocationDto struct {
+	City    string `json:"city"`
+	State   string `json:"state"`
+	Country string `json:"country"`
+}
+
+type AnalyticsDto struct {
+	Like    int `json:"like"`
+	Share   int `json:"share"`
+	Comment int `json:"comment"`
+}
+
+type CommentDto struct {
+	CommentId string `json:"commentId"`
+	UserId    string `json:"userId"`
+}
+
+type PaginationDto struct {
+	NextCursor string `json:"nextCursor,omitempty"`
+	HasMore    bool   `json:"hasMore"`
+}
